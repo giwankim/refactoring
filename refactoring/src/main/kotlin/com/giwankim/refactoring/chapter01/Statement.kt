85bb4df74561d4f47b4cdae24ae6558aa1093335
Giwan Kim <giwan.kim@gmail.com>
2025-01-18 10:53:40 +0900

    refactor: use _Slide Statements_ to move the declaration of variable `volumeCredits` next to the loop

--- refactoring/src/main/kotlin/com/giwankim/refactoring/chapter01/Statement.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/chapter01/Statement.kt
@@ -44,13 +44,13 @@ fun statement(
     fun usd(aNumber: Int): String = NumberFormat.getCurrencyInstance(Locale.US).format(aNumber / 100.0)
 
     var totalAmount = 0
-    var volumeCredits = 0
     val result = StringBuilder().apply { appendLine("Statement for ${invoice.customer}") }
     for (perf in invoice.performances) {
         // print line for this order
         result.appendLine("  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)")
         totalAmount += amountFor(perf)
     }
+    var volumeCredits = 0
     for (perf in invoice.performances) {
         volumeCredits += volumeCreditsFor(perf)
     }
