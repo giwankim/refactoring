c3693682982d11856523cd3334639687ddb34261
Giwan Kim <giwan.kim@gmail.com>
2025-02-18 18:03:54 +0900

    test: setter for `Producer.production`

--- refactoring/src/test/kotlin/com/giwankim/refactoring/chapter04/ProvinceTest.kt
+++ refactoring/src/test/kotlin/com/giwankim/refactoring/chapter04/ProvinceTest.kt
@@ -1,16 +1,18 @@
 package com.giwankim.refactoring.chapter04
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class ProvinceTest :
     FunSpec({
-        context("provide") {
-            lateinit var asia: Province
+        lateinit var asia: Province
 
-            beforeEach {
-                asia = Province.create(sampleProvinceData())
-            }
+        beforeEach {
+            asia = Province.create(sampleProvinceData())
+        }
+
+        context("provide") {
 
             test("shortfall") {
                 asia.shortfall shouldBe 5
@@ -18,6 +20,16 @@ class ProvinceTest :
 
             test("profit") {
                 asia.profit shouldBe 230
+            }
+        }
+
+        context("province") {
+            test("change production") {
+                asia.producers[0].production = 20
+                assertSoftly {
+                    asia.shortfall shouldBe -6
+                    asia.profit shouldBe 292
+                }
             }
         }
     })
