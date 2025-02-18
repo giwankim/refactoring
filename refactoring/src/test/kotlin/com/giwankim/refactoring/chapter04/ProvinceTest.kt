05d7f06bef72addcdb9961fac0cdd2c0f3ee5a45
Giwan Kim <giwan.kim@gmail.com>
2025-02-18 20:04:10 +0900

    test: boundary conditions

--- refactoring/src/test/kotlin/com/giwankim/refactoring/chapter04/ProvinceTest.kt
+++ refactoring/src/test/kotlin/com/giwankim/refactoring/chapter04/ProvinceTest.kt
@@ -1,18 +1,16 @@
 package com.giwankim.refactoring.chapter04
 
-import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class ProvinceTest :
     FunSpec({
-        lateinit var asia: Province
+        context("province") {
+            lateinit var asia: Province
 
-        beforeEach {
-            asia = Province.create(sampleProvinceData())
-        }
-
-        context("provide") {
+            beforeEach {
+                asia = Province.create(sampleProvinceData())
+            }
 
             test("shortfall") {
                 asia.shortfall shouldBe 5
@@ -21,15 +19,40 @@ class ProvinceTest :
             test("profit") {
                 asia.profit shouldBe 230
             }
-        }
 
-        context("province") {
             test("change production") {
                 asia.producers[0].production = 20
-                assertSoftly {
-                    asia.shortfall shouldBe -6
-                    asia.profit shouldBe 292
-                }
+                asia.shortfall shouldBe -6
+                asia.profit shouldBe 292
+            }
+
+            test("zero demand") {
+                asia.demand = 0
+                asia.shortfall shouldBe -25
+                asia.profit shouldBe 0
+            }
+
+            test("negative demand") {
+                asia.demand = -1
+                asia.shortfall shouldBe -26
+                asia.profit shouldBe -10
+            }
+        }
+
+        context("no producers") {
+            lateinit var noProducers: Province
+
+            beforeEach {
+                val data = ProvinceData(name = "No producers", producers = emptyList(), demand = 30, price = 20)
+                noProducers = Province.create(data)
+            }
+
+            test("shortfall") {
+                noProducers.shortfall shouldBe 30
+            }
+
+            test("profit") {
+                noProducers.profit shouldBe 0
             }
         }
     })
