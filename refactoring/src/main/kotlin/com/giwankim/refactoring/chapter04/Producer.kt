7abaa8f1c07ac487da2663dba5dffbcb4498861f
Giwan Kim <giwan.kim@gmail.com>
2025-02-18 14:45:37 +0900

    feat: add chapter 4 sample code to test

---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/chapter04/Producer.kt
@@ -0,0 +1,23 @@
+package com.giwankim.refactoring.chapter04
+
+class Producer(
+    val province: Province,
+    val name: String,
+    var cost: Int,
+    production: Int = 0,
+) {
+    private var _production: Int = production
+    var production: Int
+        get() = _production
+        set(value) {
+            province.totalProduction += value - _production
+            _production = value
+        }
+
+    companion object {
+        fun create(
+            province: Province,
+            data: ProducerData,
+        ): Producer = Producer(province, data.name, data.cost, data.production)
+    }
+}
---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/chapter04/Province.kt
@@ -0,0 +1,46 @@
+package com.giwankim.refactoring.chapter04
+
+class Province(
+    var name: String,
+    var demand: Int,
+    var price: Int,
+) {
+    var totalProduction = 0
+    private val _producers = mutableListOf<Producer>()
+    val producers: List<Producer>
+        get() = _producers.toList()
+    val shortfall: Int
+        get() = demand - totalProduction
+    val profit: Int
+        get() = demandValue - demandCost
+    val demandCost: Int
+        get() {
+            var remainingDemand = demand
+            var result = 0
+            _producers
+                .sortedBy { it.cost }
+                .forEach { producer ->
+                    val contribution = minOf(remainingDemand, producer.production)
+                    remainingDemand -= contribution
+                    result += contribution * producer.cost
+                }
+            return result
+        }
+    val demandValue: Int
+        get() = satisfiedDemand * price
+    val satisfiedDemand: Int
+        get() = minOf(demand, totalProduction)
+
+    fun addProducer(producer: Producer) {
+        _producers.add(producer)
+        totalProduction += producer.production
+    }
+
+    companion object {
+        fun create(document: ProvinceData): Province {
+            val province = Province(document.name, document.demand, document.price)
+            val producers = document.producers.map { Producer.create(province, it) }
+            return province.apply { producers.forEach { addProducer(it) } }
+        }
+    }
+}
