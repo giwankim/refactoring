a75ce692489c0825bc64845d45ab07d8879ee628
Giwan Kim <giwan.kim@gmail.com>
2025-02-27 11:45:51 +0900

    refactor: introduce `range` as a parameter

---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/chapter06/introduce/parameter/objects/refactor/IntroduceParameterObject.kt
@@ -0,0 +1,11 @@
+package com.giwankim.refactoring.chapter06.introduce.parameter.objects.refactor
+
+import com.giwankim.refactoring.chapter06.introduce.parameter.objects.Reading
+import com.giwankim.refactoring.chapter06.introduce.parameter.objects.Station
+
+fun readingsOutsideRange(
+    station: Station,
+    min: Int,
+    max: Int,
+    range: NumberRange,
+): List<Reading> = station.readings.filter { it.temp in min..max }
---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/chapter06/introduce/parameter/objects/refactor/NumberRange.kt
@@ -0,0 +1,6 @@
+package com.giwankim.refactoring.chapter06.introduce.parameter.objects.refactor
+
+data class NumberRange(
+    val min: Int,
+    val max: Int,
+)
