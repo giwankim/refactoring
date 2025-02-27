a18b867c5db18936ae211e891a481115231f1b64
Giwan Kim <giwan.kim@gmail.com>
2025-02-27 12:01:13 +0900

    fix: outside range not inside!!

--- refactoring/src/main/kotlin/com/giwankim/refactoring/chapter06/introduce/parameter/objects/Example.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/chapter06/introduce/parameter/objects/Example.kt
@@ -4,4 +4,4 @@ fun readingsOutsideRange(
     station: Station,
     min: Int,
     max: Int,
-): List<Reading> = station.readings.filter { it.temp in min..max }
+): List<Reading> = station.readings.filter { it.temp !in min..max }
