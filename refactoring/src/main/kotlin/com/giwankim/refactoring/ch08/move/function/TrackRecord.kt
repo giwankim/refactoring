596c9268696cb798d0ff2bcf74c361af1c27aacb
Giwan Kim <giwan.kim@gmail.com>
2025-03-10 16:18:20 +0900

    refactor: remove dead code

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/move/function/TrackRecord.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/move/function/TrackRecord.kt
@@ -28,27 +28,7 @@ fun trackSummary(points: List<Point>): TrackSummary {
         return Duration.between(start, end)
     }
 
-    fun calculateDistance(): Double {
-        fun radians(degrees: Double): Double = degrees * Math.PI / 180
-
-        fun distance(
-            p1: Point,
-            p2: Point,
-        ): Double {
-            // haversine formula see http://www.movable-type.co.uk/scripts/latlong.html
-            val earthRadius = 3959 // in miles
-            val dLat = radians(p2.lat) - radians(p1.lat)
-            val dLon = radians(p2.lon) - radians(p1.lon)
-            val a =
-                sin(dLat / 2).pow(2) +
-                    cos(radians(p1.lat)) * cos(radians(p2.lat)) *
-                    sin(dLon / 2) * sin(dLon / 2)
-            val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-            return earthRadius * c
-        }
-
-        return topCalculateDistance(points)
-    }
+    fun calculateDistance(): Double = topCalculateDistance(points)
 
     val totalTime = calculateTime()
     val totalDistance = calculateDistance()
