1d45875dd35bcd4c1ea0e70ef469d595f8a72f44
Giwan Kim <giwan.kim@gmail.com>
2025-03-11 16:47:16 +0900

    refactor: replace splitting a line with map operation

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/replace/loop/with/pipeline/ReplaceLoopWithPipeline.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/replace/loop/with/pipeline/ReplaceLoopWithPipeline.kt
@@ -3,9 +3,13 @@ package com.giwankim.refactoring.ch08.replace.loop.with.pipeline
 fun acquireData(input: String): List<Office> {
     val lines = input.lines()
     val result: MutableList<Office> = mutableListOf()
-    val loopItems = lines.drop(1).filter { it.isNotBlank() }
+    val loopItems =
+        lines
+            .drop(1)
+            .filter { it.isNotBlank() }
+            .map { it.split(",") }
     for (line in loopItems) {
-        val record = line.split(",")
+        val record = line
         if (record[1].trim() == "India") {
             result.add(Office(city = record[0].trim(), phone = record[2].trim()))
         }
