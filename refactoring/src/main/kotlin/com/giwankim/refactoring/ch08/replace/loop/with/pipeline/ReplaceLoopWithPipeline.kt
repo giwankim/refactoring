9893bbf610984c568055f2794a0797845ea242ac
Giwan Kim <giwan.kim@gmail.com>
2025-03-11 16:54:21 +0900

    refactor: polish

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/replace/loop/with/pipeline/ReplaceLoopWithPipeline.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/replace/loop/with/pipeline/ReplaceLoopWithPipeline.kt
@@ -2,16 +2,10 @@ package com.giwankim.refactoring.ch08.replace.loop.with.pipeline
 
 fun acquireData(input: String): List<Office> {
     val lines = input.lines()
-    val result: MutableList<Office> = mutableListOf()
-    val loopItems =
-        lines
-            .drop(1)
-            .filter { it.isNotBlank() }
-            .map { it.split(",") }
-            .filter { it[1].trim() == "India" }
-            .map { Office(city = it[0].trim(), phone = it[2].trim()) }
-    for (line in loopItems) {
-        result.add(line)
-    }
-    return result
+    return lines
+        .drop(1)
+        .filter { line -> line.isNotBlank() }
+        .map { line -> line.split(",") }
+        .filter { fields -> fields[1].trim() == "India" }
+        .map { fields -> Office(city = fields[0].trim(), phone = fields[2].trim()) }
 }
