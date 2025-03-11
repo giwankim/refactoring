85df7d866ba7a5c1f4f8f8f09fb8607e9186c857
Giwan Kim <giwan.kim@gmail.com>
2025-03-11 10:25:31 +0900

    refactor: remove interest rate field from account

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/move/field/Account.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/move/field/Account.kt
@@ -3,9 +3,7 @@ package com.giwankim.refactoring.ch08.move.field
 class Account(
     val number: Long,
     val type: AccountType,
-    val interestRate: Double,
 ) {
-    init {
-        assert(interestRate == type.interestRate) { "Interest rate mismatch" }
-    }
+    val interestRate: Double
+        get() = type.interestRate
 }
