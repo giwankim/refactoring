4bf60921808d9482a39ccf8dc638c6a34c31659c
Giwan Kim <giwan.kim@gmail.com>
2025-03-11 10:12:53 +0900

    feat: add _Moving to a Shared Object_ example

---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/move/field/Account.kt
@@ -0,0 +1,7 @@
+package com.giwankim.refactoring.ch08.move.field
+
+class Account(
+    val number: Long,
+    val type: AccountType,
+    val interestRate: Double,
+)
---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/move/field/AccountType.kt
@@ -0,0 +1,5 @@
+package com.giwankim.refactoring.ch08.move.field
+
+class AccountType(
+    val nameString: String,
+)
