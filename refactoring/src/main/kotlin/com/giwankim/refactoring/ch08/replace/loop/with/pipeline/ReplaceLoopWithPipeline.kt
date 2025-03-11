07554a2fc43c559234ae75eb82756f77f656fa86
Giwan Kim <giwan.kim@gmail.com>
2025-03-11 16:34:21 +0900

    refactor: replace removing blank lines with filter operation

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/replace/loop/with/pipeline/ReplaceLoopWithPipeline.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/replace/loop/with/pipeline/ReplaceLoopWithPipeline.kt
@@ -3,11 +3,8 @@ package com.giwankim.refactoring.ch08.replace.loop.with.pipeline
 fun acquireData(input: String): List<Office> {
     val lines = input.lines()
     val result: MutableList<Office> = mutableListOf()
-    val loopItems = lines.drop(1)
+    val loopItems = lines.drop(1).filter { it.isNotBlank() }
     for (line in loopItems) {
-        if (line.isBlank()) {
-            continue
-        }
         val record = line.split(",")
         if (record[1].trim() == "India") {
             result.add(Office(city = record[0].trim(), phone = record[2].trim()))
