cbefd8ee4d5823ebfac6cfedca6ba23fe59df959
Giwan Kim <giwan.kim@gmail.com>
2025-03-11 15:00:23 +0900

    refactor: Slide Statements

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/split/loop/SplitLoop.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/split/loop/SplitLoop.kt
@@ -1,12 +1,12 @@
 package com.giwankim.refactoring.ch08.split.loop
 
 fun process(people: List<Person>): String {
-    var youngest = if (people[0] != null) people[0].age else Int.MAX_VALUE
     var totalSalary = 0
     for (p in people) {
         totalSalary += p.salary
     }
 
+    var youngest = if (people[0] != null) people[0].age else Int.MAX_VALUE
     for (p in people) {
         if (p.age < youngest) {
             youngest = p.age
