7929ed1c25498fb68e6de54048234bda10d422c6
Giwan Kim <giwan.kim@gmail.com>
2025-03-12 12:17:04 +0900

    feat: add example for "Slide Statements"

---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch08/slide/statements/SlideStatements.kt
@@ -0,0 +1,41 @@
+package com.giwankim.refactoring.ch08.slide.statements
+
+import kotlin.math.max
+
+class Order(
+    val units: Int,
+    val isRepeat: Boolean,
+)
+
+class PricingPlan(
+    val base: Double,
+    val unit: Double,
+    val discountThreshold: Int,
+    val discountFactor: Double,
+)
+
+fun main() {
+    val pricingPlan = retrievePricingPlan()
+    val order = retrieveOrder()
+    val baseCharge = pricingPlan.base
+    var charge: Double
+    val chargePerUnit = pricingPlan.unit
+    val units = order.units
+    var discount: Double
+    charge = baseCharge + units * chargePerUnit
+    var discountableUnits = max(units - pricingPlan.discountThreshold, 0)
+    discount = discountableUnits * pricingPlan.discountFactor
+    if (order.isRepeat) {
+        discount += 20
+    }
+    charge = charge - discount
+    chargeOrder(charge)
+}
+
+fun retrievePricingPlan() = PricingPlan(base = 10.0, unit = 2.0, discountThreshold = 5, discountFactor = 0.1)
+
+fun retrieveOrder() = Order(units = 1, isRepeat = false)
+
+fun chargeOrder(charge: Double) {
+    println("Charge: $charge")
+}
