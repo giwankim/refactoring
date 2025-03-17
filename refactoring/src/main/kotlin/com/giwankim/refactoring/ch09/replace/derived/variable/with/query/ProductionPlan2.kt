9677cf6cc2f39cbbebf99a5d192e1ef15a6cc6ca
Giwan Kim <giwan.kim@gmail.com>
2025-03-17 13:29:43 +0900

    refactor: remove accumulator

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan2.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan2.kt
@@ -4,13 +4,11 @@ class ProductionPlan2(
     production: Int,
 ) {
     private var initialProduction: Int = production
-    private var productionAccumulator: Int = 0
     private val adjustments: MutableList<Adjustment> = mutableListOf()
     val production: Int
-        get() = initialProduction + adjustments.sumOf<Adjustment> { it.amount }
+        get() = initialProduction + adjustments.sumOf { it.amount }
 
     fun applyAdjustment(adjustment: Adjustment) {
         adjustments.add(adjustment)
-        productionAccumulator += adjustment.amount
     }
 }
