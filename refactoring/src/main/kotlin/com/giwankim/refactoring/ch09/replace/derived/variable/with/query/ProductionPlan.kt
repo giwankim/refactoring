2141646b8816e956a0408cd00726a9bdcd491ec6
Giwan Kim <giwan.kim@gmail.com>
2025-03-17 13:12:18 +0900

    refactor: Remove Dead Code

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
@@ -1,13 +1,11 @@
 package com.giwankim.refactoring.ch09.replace.derived.variable.with.query
 
 class ProductionPlan {
-    private var _production: Int = 0
     val production: Int
         get() = adjustments.sumOf<Adjustment> { it.amount }
     private val adjustments: MutableList<Adjustment> = mutableListOf()
 
     fun applyAdjustment(adjustment: Adjustment) {
         adjustments.add(adjustment)
-        _production += adjustment.amount
     }
 }
