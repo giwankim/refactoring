c5d767e527d098b60bdb5bc4d5da6a7c188bdb9e
Giwan Kim <giwan.kim@gmail.com>
2025-03-17 13:07:12 +0900

    refactor: replace returning field with returning the calculation

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
@@ -3,10 +3,7 @@ package com.giwankim.refactoring.ch09.replace.derived.variable.with.query
 class ProductionPlan {
     private var _production: Int = 0
     val production: Int
-        get() {
-            check(_production == calculatedProduction)
-            return _production
-        }
+        get() = calculatedProduction
     val calculatedProduction: Int
         get() = adjustments.sumOf { it.amount }
     private val adjustments: MutableList<Adjustment> = mutableListOf()
