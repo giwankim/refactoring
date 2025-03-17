b2b06ff0797c36929a79a54ef6530f07389788d3
Giwan Kim <giwan.kim@gmail.com>
2025-03-17 13:11:37 +0900

    refactor: inline function `calculatedProduction`

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
@@ -3,9 +3,7 @@ package com.giwankim.refactoring.ch09.replace.derived.variable.with.query
 class ProductionPlan {
     private var _production: Int = 0
     val production: Int
-        get() = calculatedProduction
-    val calculatedProduction: Int
-        get() = adjustments.sumOf { it.amount }
+        get() = adjustments.sumOf<Adjustment> { it.amount }
     private val adjustments: MutableList<Adjustment> = mutableListOf()
 
     fun applyAdjustment(adjustment: Adjustment) {
