25e61bc0bc5630edbb947c5ab03de25ee510dfee
Giwan Kim <giwan.kim@gmail.com>
2025-03-17 13:05:42 +0900

    refactor: Introduce Assertion on `calculatedProduction`

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/replace/derived/variable/with/query/ProductionPlan.kt
@@ -3,7 +3,12 @@ package com.giwankim.refactoring.ch09.replace.derived.variable.with.query
 class ProductionPlan {
     private var _production: Int = 0
     val production: Int
-        get() = _production
+        get() {
+            check(_production == calculatedProduction)
+            return _production
+        }
+    val calculatedProduction: Int
+        get() = adjustments.sumOf { it.amount }
     private val adjustments: MutableList<Adjustment> = mutableListOf()
 
     fun applyAdjustment(adjustment: Adjustment) {
