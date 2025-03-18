038ebd4de11dd1f9f026f87142828ed8a0dec052
Giwan Kim <giwan.kim@gmail.com>
2025-03-18 10:39:17 +0900

    feat: add "Change Value to Reference" example

---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/change/value/to/reference/Customer.kt
@@ -0,0 +1,5 @@
+package com.giwankim.refactoring.ch09.change.value.to.reference
+
+class Customer {
+    var id: Long? = null
+}
---
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch09/change/value/to/reference/Order.kt
@@ -0,0 +1,6 @@
+package com.giwankim.refactoring.ch09.change.value.to.reference
+
+class Order(
+    val number: Long,
+    val customer: Customer,
+)
