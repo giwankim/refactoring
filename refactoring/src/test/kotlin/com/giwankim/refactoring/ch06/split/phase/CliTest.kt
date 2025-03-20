8a325eda32bf525300649c15d3118ce556f1f6bf
Giwan Kim <giwan.kim@gmail.com>
2025-03-20 17:09:35 +0900

    refactor: Move Statements to Callers on the call to System.out

--- refactoring/src/test/kotlin/com/giwankim/refactoring/ch06/split/phase/CliTest.kt
+++ refactoring/src/test/kotlin/com/giwankim/refactoring/ch06/split/phase/CliTest.kt
@@ -1,10 +1,12 @@
 package com.giwankim.refactoring.ch06.split.phase
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.PrintStream
+import java.lang.IllegalArgumentException
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 
@@ -20,22 +22,41 @@ class CliTest :
             tempFile.delete()
         }
 
-        test("count number of orders") {
-            tempFile.writeText("""[{"status": "ready"}, {"status": "pending"}, {"status": "completed"}]""")
-            val output =
-                capturingStdOut {
-                    main(arrayOf(tempFile.absolutePath))
-                }
-            output.trim().toInt() shouldBe 3
+        context("run") {
+            test("exit with error when no filename is supplied") {
+                val exception = shouldThrow<IllegalArgumentException> { run(emptyArray()) }
+                exception.message shouldBe "must supply a filename"
+            }
+
+            test("count number of orders") {
+                tempFile.writeText("""[{"status": "ready"}, {"status": "pending"}, {"status": "completed"}]""")
+                run(arrayOf(tempFile.absolutePath)) shouldBe 3
+            }
+
+            test("count number of orders whose status is READY") {
+                tempFile.writeText("""[{"status": "ready"}, {"status": "pending"}, {"status": "ready"}]""")
+                run(arrayOf("-r", tempFile.absolutePath)) shouldBe 2
+            }
         }
 
-        test("count only the orders whose status is ready") {
-            tempFile.writeText("""[{"status": "ready"}, {"status": "pending"}, {"status": "ready"}]""")
-            val output =
-                capturingStdOut {
-                    main(arrayOf("-r", tempFile.absolutePath))
-                }
-            output.trim().toInt() shouldBe 2
+        context("main") {
+            test("count number of orders") {
+                tempFile.writeText("""[{"status": "ready"}, {"status": "pending"}, {"status": "completed"}]""")
+                val output =
+                    capturingStdOut {
+                        main(arrayOf(tempFile.absolutePath))
+                    }
+                output.trim().toInt() shouldBe 3
+            }
+
+            test("count only the orders whose status is ready") {
+                tempFile.writeText("""[{"status": "ready"}, {"status": "pending"}, {"status": "ready"}]""")
+                val output =
+                    capturingStdOut {
+                        main(arrayOf("-r", tempFile.absolutePath))
+                    }
+                output.trim().toInt() shouldBe 2
+            }
         }
     })
 
