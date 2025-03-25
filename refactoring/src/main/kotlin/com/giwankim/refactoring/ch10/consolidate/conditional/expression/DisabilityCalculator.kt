0f3d8cd9144e6417e99dff8bc4ea6209aac4fd4c
Giwan Kim <giwan.kim@gmail.com>
2025-03-25 10:15:22 +0900

    refactor: fold the conditional on `isPartTime` with the rest

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
@@ -1,9 +1,8 @@
 package com.giwankim.refactoring.ch10.consolidate.conditional.expression
 
 fun disabilityAmount(anEmployee: Employee): Int {
-    if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12) {
+    if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime) {
         return 0
     }
-    if (anEmployee.isPartTime) return 0
     return 1
 }
