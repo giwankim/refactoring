14f31a891ec26ecf03dd57aeb1889ee838c491d4
Giwan Kim <giwan.kim@gmail.com>
2025-03-25 17:44:19 +0900

    refactor: guard clause returns zero; inline return clause

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/replace/nested/conditional/with/guard/clauses/Capital.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/replace/nested/conditional/with/guard/clauses/Capital.kt
@@ -1,10 +1,8 @@
 package com.giwankim.refactoring.ch10.replace.nested.conditional.with.guard.clauses
 
 fun adjustedCapital(instrument: Instrument): Double {
-    var result = 0.0
     if (instrument.capital <= 0.0 || instrument.interestRate <= 0.0 || instrument.duration <= 0.0) {
-        return result
+        return 0.0
     }
-    result = (instrument.income / instrument.duration) * instrument.adjustmentFactor
-    return result
+    return (instrument.income / instrument.duration) * instrument.adjustmentFactor
 }
