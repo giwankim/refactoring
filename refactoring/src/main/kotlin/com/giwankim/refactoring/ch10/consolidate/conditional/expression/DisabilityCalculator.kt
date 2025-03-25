3acb8488d1fa19a52a1aa7b7ae408a3a9e58f95c
Giwan Kim <giwan.kim@gmail.com>
2025-03-25 10:12:47 +0900

    refactor: fold the conditional on `monthsDisabled` with the first conditional

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
@@ -1,8 +1,9 @@
 package com.giwankim.refactoring.ch10.consolidate.conditional.expression
 
 fun disabilityAmount(anEmployee: Employee): Int {
-    if (anEmployee.seniority < 2) return 0
-    if (anEmployee.monthsDisabled > 12) return 0
+    if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12) {
+        return 0
+    }
     if (anEmployee.isPartTime) return 0
     return 1
 }
