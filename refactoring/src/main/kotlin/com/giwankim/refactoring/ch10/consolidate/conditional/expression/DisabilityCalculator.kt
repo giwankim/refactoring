ed7e39245554cd87e1d60434d4d61b3215d2fc50
Giwan Kim <giwan.kim@gmail.com>
2025-03-25 10:17:43 +0900

    fix: typo

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
@@ -1,11 +1,11 @@
 package com.giwankim.refactoring.ch10.consolidate.conditional.expression
 
 fun disabilityAmount(anEmployee: Employee): Int {
-    if (isNotEligableForDisability(anEmployee)) {
+    if (isNotEligibleForDisability(anEmployee)) {
         return 0
     }
     return 1
 }
 
-private fun isNotEligableForDisability(anEmployee: Employee): Boolean =
+private fun isNotEligibleForDisability(anEmployee: Employee): Boolean =
     anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime
