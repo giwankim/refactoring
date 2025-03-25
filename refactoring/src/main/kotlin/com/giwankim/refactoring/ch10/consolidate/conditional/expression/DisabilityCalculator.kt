fb428a8bd94f78a532e0d832c911bff85d1abc21
Giwan Kim <giwan.kim@gmail.com>
2025-03-25 10:16:25 +0900

    refactor: "Extract Function" on the entire conditional

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/consolidate/conditional/expression/DisabilityCalculator.kt
@@ -1,8 +1,11 @@
 package com.giwankim.refactoring.ch10.consolidate.conditional.expression
 
 fun disabilityAmount(anEmployee: Employee): Int {
-    if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime) {
+    if (isNotEligableForDisability(anEmployee)) {
         return 0
     }
     return 1
 }
+
+private fun isNotEligableForDisability(anEmployee: Employee): Boolean =
+    anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime
