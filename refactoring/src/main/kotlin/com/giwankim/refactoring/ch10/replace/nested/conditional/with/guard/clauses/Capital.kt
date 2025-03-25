08c5061f71cbb3905290fc4413afef861a5f256f
Giwan Kim <giwan.kim@gmail.com>
2025-03-25 17:39:51 +0900

    refactor: Consolidate Conditional Expression

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/replace/nested/conditional/with/guard/clauses/Capital.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/replace/nested/conditional/with/guard/clauses/Capital.kt
@@ -2,10 +2,7 @@ package com.giwankim.refactoring.ch10.replace.nested.conditional.with.guard.clau
 
 fun adjustedCapital(instrument: Instrument): Double {
     var result = 0.0
-    if (instrument.capital <= 0.0) {
-        return result
-    }
-    if (instrument.interestRate <= 0.0 || instrument.duration <= 0.0) {
+    if (instrument.capital <= 0.0 || instrument.interestRate <= 0.0 || instrument.duration <= 0.0) {
         return result
     }
     result = (instrument.income / instrument.duration) * instrument.adjustmentFactor
