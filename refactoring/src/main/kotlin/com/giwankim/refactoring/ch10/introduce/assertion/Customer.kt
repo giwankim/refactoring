d787cb068f31c06d0c0bfb206616880cdd5fb01c
Giwan Kim <giwan.kim@gmail.com>
2025-03-25 10:46:28 +0900

    refactor: add assertions into constructor and setter

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/introduce/assertion/Customer.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch10/introduce/assertion/Customer.kt
@@ -1,7 +1,22 @@
 package com.giwankim.refactoring.ch10.introduce.assertion
 
 class Customer(
-    val discountRate: Double,
+    private var _discountRate: Double,
 ) {
+    var discountRate: Double
+        get() = _discountRate
+        set(value) {
+            requirePositive(value)
+            _discountRate = value
+        }
+
+    init {
+        requirePositive(_discountRate)
+    }
+
     fun applyDiscount(number: Double): Double = if (discountRate != 0.0) number - (discountRate * number) else number
+
+    private fun requirePositive(value: Double) {
+        require(value >= 0.0) { "discountRate should be positive" }
+    }
 }
--- refactoring/src/test/kotlin/com/giwankim/refactoring/ch10/introduce/assertion/CustomerTest.kt
+++ refactoring/src/test/kotlin/com/giwankim/refactoring/ch10/introduce/assertion/CustomerTest.kt
@@ -1,10 +1,27 @@
 package com.giwankim.refactoring.ch10.introduce.assertion
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class CustomerTest :
     FunSpec({
+        context("customer") {
+            test("constructor discountRate is negative then throw exception") {
+                shouldThrow<IllegalArgumentException> { Customer(-0.1) }
+            }
+
+            test("set discountRate is negative then throw exception") {
+                val customer = Customer(0.0)
+                shouldThrow<IllegalArgumentException> { customer.discountRate = -0.1 }
+            }
+
+            test("set discountRate to positive value") {
+                val customer = Customer(0.2)
+                customer.discountRate shouldBe 0.2
+            }
+        }
+
         context("applyDiscount") {
             test("discountRate is 0.0 then no discount") {
                 val customer = Customer(0.0)
