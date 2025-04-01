58474b6588693846d1357307cc3a89063a30c37a
Giwan Kim <giwan.kim@gmail.com>
2025-04-01 12:06:08 +0900

    refactor: replace similar functions with parametrized function `raise()`

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/parametrize/function/Salary.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/parametrize/function/Salary.kt
@@ -1,9 +1,16 @@
 package com.giwankim.refactoring.ch11.parametrize.function
 
 fun tenPercentRaise(person: Person) {
-    person.salary *= 1.1
+    raise(person, 0.1)
 }
 
 fun fivePercentRaise(person: Person) {
-    person.salary *= 1.05
+    raise(person, 0.05)
+}
+
+fun raise(
+    person: Person,
+    factor: Double,
+) {
+    person.salary *= 1 + factor
 }
