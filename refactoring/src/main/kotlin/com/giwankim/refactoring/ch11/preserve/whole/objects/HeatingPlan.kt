278dbf19c8fe7404f263c59979aede7b242a00a5
Giwan Kim <giwan.kim@gmail.com>
2025-04-01 15:07:57 +0900

    refactor: inline `withinRange()` function

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/preserve/whole/objects/HeatingPlan.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/preserve/whole/objects/HeatingPlan.kt
@@ -3,10 +3,5 @@ package com.giwankim.refactoring.ch11.preserve.whole.objects
 class HeatingPlan(
     val temperatureRange: Range,
 ) {
-    fun withinRange(
-        bottom: Int,
-        top: Int,
-    ): Boolean = temperatureRange.low >= bottom && temperatureRange.high <= top
-
-    fun xxNEWwithinRange(range: Range): Boolean = withinRange(bottom = range.low, top = range.high)
+    fun xxNEWwithinRange(range: Range): Boolean = temperatureRange.low >= range.low && temperatureRange.high <= range.high
 }
