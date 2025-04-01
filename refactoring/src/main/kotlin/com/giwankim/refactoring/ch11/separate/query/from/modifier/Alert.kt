b68a73f22448c655fe8e614e9e417a7f9b498414
Giwan Kim <giwan.kim@gmail.com>
2025-04-01 11:14:20 +0900

    refactor: copy `alertForMiscreant()` and name it as the query function `findMiscreant()`

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/separate/query/from/modifier/Alert.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/separate/query/from/modifier/Alert.kt
@@ -14,6 +14,20 @@ fun alertForMiscreant(people: List<Person>): Person? {
     return null
 }
 
+fun findMiscreant(people: List<Person>): Person? {
+    for (p in people) {
+        if (p.name == "Don") {
+            setOffAlarms()
+            return p
+        }
+        if (p.name == "John") {
+            setOffAlarms()
+            return p
+        }
+    }
+    return null
+}
+
 fun setOffAlarms() {
     // set off alarms
 }
