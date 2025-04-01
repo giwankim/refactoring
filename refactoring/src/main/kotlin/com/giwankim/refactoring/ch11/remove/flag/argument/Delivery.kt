0f875a95328137580e9bd2d28a7e3ef9016d1459
Giwan Kim <giwan.kim@gmail.com>
2025-04-01 13:52:52 +0900

    refactor: separate out `rushDeliveryDate()` and `regularDeliveryDate()`
    apply "Decompose Conditional"

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/remove/flag/argument/Delivery.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/remove/flag/argument/Delivery.kt
@@ -7,23 +7,31 @@ fun deliveryDate(
     isRush: Boolean,
 ): LocalDateTime =
     if (isRush) {
-        var deliveryTime: Long
-        if (order.deliveryState in listOf("MA", "CT")) {
-            deliveryTime = 1L
-        } else if (order.deliveryState in listOf("NY", "NJ")) {
-            deliveryTime = 2L
-        } else {
-            deliveryTime = 3L
-        }
-        order.placedOn.plusDays(1 + deliveryTime)
+        rushDeliveryDate(order)
     } else {
-        var deliveryTime: Long
-        if (order.deliveryState in listOf("MA", "CT", "NY")) {
-            deliveryTime = 2L
-        } else if (order.deliveryState in listOf("ME", "NH")) {
-            deliveryTime = 3L
-        } else {
-            deliveryTime = 4L
-        }
-        order.placedOn.plusDays(2 + deliveryTime)
+        regularDeliveryDate(order)
     }
+
+private fun rushDeliveryDate(order: Order): LocalDateTime {
+    var deliveryTime: Long
+    if (order.deliveryState in listOf("MA", "CT")) {
+        deliveryTime = 1L
+    } else if (order.deliveryState in listOf("NY", "NJ")) {
+        deliveryTime = 2L
+    } else {
+        deliveryTime = 3L
+    }
+    return order.placedOn.plusDays(1 + deliveryTime)
+}
+
+private fun regularDeliveryDate(order: Order): LocalDateTime {
+    var deliveryTime: Long
+    if (order.deliveryState in listOf("MA", "CT", "NY")) {
+        deliveryTime = 2L
+    } else if (order.deliveryState in listOf("ME", "NH")) {
+        deliveryTime = 3L
+    } else {
+        deliveryTime = 4L
+    }
+    return order.placedOn.plusDays(2 + deliveryTime)
+}
