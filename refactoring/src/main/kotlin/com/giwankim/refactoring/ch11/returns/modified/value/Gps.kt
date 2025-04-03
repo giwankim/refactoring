726c6038329cbb0d14f9f19c8eb48232950b206c
Giwan Kim <giwan.kim@gmail.com>
2025-04-03 17:46:08 +0900

    refactor: apply "Return Modified Value" to `totalDistance`

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/returns/modified/value/Gps.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/returns/modified/value/Gps.kt
@@ -3,8 +3,6 @@ package com.giwankim.refactoring.ch11.returns.modified.value
 fun main() {
     val points = listOf(Point(100), Point(200), Point(150))
 
-    var totalDistance = 0
-
     fun calculateAscent(): Int {
         var result = 0
         for (i in 1 until points.size) {
@@ -20,12 +18,14 @@ fun main() {
         return totalTime
     }
 
-    fun calculateDistance() {
+    fun calculateDistance(): Int {
+        var totalDistance = 0
         // Assuming each point has a distance property
+        return totalDistance
     }
 
     val totalAscent = calculateAscent()
     val totalTime = calculateTime()
-    calculateDistance()
+    val totalDistance = calculateDistance()
     val pace = totalTime / 60.0 / totalDistance
 }
