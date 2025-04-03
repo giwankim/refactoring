9564b7554be97ba4a4362c4144cd3e5ca59d1b29
Giwan Kim <giwan.kim@gmail.com>
2025-04-03 13:51:49 +0900

    refactor: add `Scorer` and apply "Move Function" to move the body of `score()` into `Scorer#execute()`

--- refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/replace/function/with/command/Score.kt
+++ refactoring/src/main/kotlin/com/giwankim/refactoring/ch11/replace/function/with/command/Score.kt
@@ -6,21 +6,29 @@ fun score(
     candidate: Candidate,
     medicalExam: MedicalExam,
     scoringGuide: ScoringGuide,
-): Int {
-    var result = 0
-    var healthLevel = 0
-    var highMedicalRiskFlag = false
+): Int = Scorer().execute(candidate, medicalExam, scoringGuide)
 
-    if (medicalExam.isSmoker) {
-        healthLevel += 10
-        highMedicalRiskFlag = true
+class Scorer {
+    fun execute(
+        candidate: Candidate,
+        medicalExam: MedicalExam,
+        scoringGuide: ScoringGuide,
+    ): Int {
+        var result = 0
+        var healthLevel = 0
+        var highMedicalRiskFlag = false
+
+        if (medicalExam.isSmoker) {
+            healthLevel += 10
+            highMedicalRiskFlag = true
+        }
+        var certificationGrade = "regular"
+        if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+            certificationGrade = "low"
+            result -= 5
+        }
+        // lots more code like this
+        result -= max(healthLevel - 5, 0)
+        return result
     }
-    var certificationGrade = "regular"
-    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-        certificationGrade = "low"
-        result -= 5
-    }
-    // lots more code like this
-    result -= max(healthLevel - 5, 0)
-    return result
 }
